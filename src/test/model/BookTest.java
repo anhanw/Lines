03556c0662c6cfe8444be50081349551d52d7da2
Anhan <anhanw@student.ubc.ca>
2022-10-16 18:57:28 -0700

    Delete one useless import line from Book class.

--- src/test/model/BookTest.java
+++ src/test/model/BookTest.java
@@ -3,7 +3,6 @@ package model;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static jdk.nashorn.internal.objects.NativeSet.size;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
